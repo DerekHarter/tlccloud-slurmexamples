f7da2fd8755d309e8293038240edecc81570130d
Derek Harter <derek@harter.pro>
2026-06-23 15:10:30 -0500

    Update 03 shared memory multicore example
    Just update the batch script to be consistent with
    and pick up enhancements made in the 02 example.

--- 03-sharedmem-multicore/randomforest-example.sh
+++ 03-sharedmem-multicore/randomforest-example.sh
@@ -4,12 +4,38 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=10G
 #SBATCH --output=randomforest-%5A.out
+
+# Display node and directory that was assigned and that we are running the job from.
 echo "Compute node: `hostname`"
 echo "Project directory: `pwd`"
+
+# Setup conda profile and ensure we are using the correct conda environment
+# for the following computation.
+# Note: you may need to adjust the path to the conda profile shell script and
+# the name of the conda environment you wish to activate here.
+. /home/miniconda3/etc/profile.d/conda.sh
+conda activate keras-tf-gpu
+echo "PATH: $PATH"
+echo "python path: `which python`"
+echo "conda path: `which conda`"
+echo "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
+echo "--------------------------------------------------"
+echo ""
+
+# Can setup other needed configuration such as environment variable
+# settings that may be needed by the computation.
+# For example, reduce tensorflow warning log level as these warning
+# are not really useful.
+export TF_CPP_MIN_LOG_LEVEL=3
+
+# Record start time to display elapsed time of computation
 echo "Start time: `date`"
 start=$(date +%s)
-source activate keras-tf-gpu
-./randomforest-example.py
+
+# Run the actual computation by invoking python script or executable
+python randomforest-example.py
+
+# Record end time to display elasped time of computation 
 echo "End time: `date`"
 end=$(date +%s)
 secs=($end-$start)
