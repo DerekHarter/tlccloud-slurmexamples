b1161c037d6340f4d9bec78fd8e9f50e311bf8d1
Derek Harter <derek@harter.pro>
2026-06-23 13:19:25 -0500

    Updage shell script to setup conda environment correctly
    Fix the batch shell script to corectly initialize and
    setup conda, and correctly activate the needed conda
    environment.
    
    Also clean up script a bit, add in more information output, and
    comment a bit better.

--- 02-python-conda/gputest.py
+++ 02-python-conda/gputest.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
-# suppress tensorflow output, not useful when running computational tasks
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-
+import sys
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -14,14 +11,16 @@ mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
 loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-print('mnist data loaded and split into train/test')
 NUM_TRIALS = 3
+
+print('')
+print('mnist data loaded and split into train/test')
 
 def test_cpu_performance():
     print('test_cpu_performance: testing using only device /CPU:0')
     with tf.device('/CPU:0'):
         model_cpu_base = tf.keras.models.Sequential([
-            tf.layers.Input(shape=(28, 28)),
+            tf.keras.layers.Input(shape=(28, 28)),
             tf.keras.layers.Flatten(),
             tf.keras.layers.Dense(1024, activation='relu'),
             tf.keras.layers.Dropout(0.2),
@@ -31,7 +30,7 @@ def test_cpu_performance():
     for trial in range(NUM_TRIALS):
         print('=' * 70)
         print('cpu performance trial: ', trial + 1)
-        
+
         with tf.device('/CPU:0'):
             model_cpu = tf.keras.models.clone_model(model_cpu_base)
             model_cpu.compile(optimizer='adam',
@@ -39,7 +38,7 @@ def test_cpu_performance():
                               metrics=['accuracy'])
 
             start = timer()
-            model_cpu.fit(x_train, y_train, epochs=5, verbose=2)
+            model_cpu.fit(x_train[:10000], y_train[:10000], epochs=5, verbose=2)
             end = timer()
             
             print('cpu elapsed time: ', end - start)
@@ -51,7 +50,7 @@ def test_gpu_performance():
     print('test_gpu_performance: testing using single gpu device /GPU:0')
     with tf.device('/GPU:0'):
         model_gpu_base = tf.keras.models.Sequential([
-            tf.layers.Input(shape=(28, 28)),
+            tf.keras.layers.Input(shape=(28, 28)),
             tf.keras.layers.Flatten(),
             tf.keras.layers.Dense(1024, activation='relu'),
             tf.keras.layers.Dropout(0.2),
@@ -75,11 +74,11 @@ def test_gpu_performance():
                 print('gpu elapsed time: ', end - start)
                 print('=' * 70)
                 print('')
-
     
 def main():
     print('Available Devices : ', tf.config.list_physical_devices())
     print('Num GPUs Available: ', len(tf.config.list_physical_devices('GPU')))    
+    print('')
 
     test_cpu_performance()
     test_gpu_performance()
--- 02-python-conda/gputest.sh
+++ 02-python-conda/gputest.sh
@@ -5,13 +5,38 @@
 #SBATCH --mem=3G
 #SBATCH --output=gputest-%5A.out
 #SBATCH --gpus=1
+
+# Display node and directory that was assigned and that we are running the job from.
 echo "Compute node: `hostname`"
 echo "Project directory: `pwd`"
+
+# Setup conda profile and ensure we are using the correct conda environment
+# for the following computation.
+# Note: you may need to adjust the path to the conda profile shell script and
+# the name of the conda environment you wish to activate here.
+. /home/miniconda3/etc/profile.d/conda.sh
+conda activate keras-tf-gpu
+echo "PATH: $PATH"
+echo "python path: `which python`"
+echo "conda path: `which conda`"
+echo "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
+echo "--------------------------------------------------"
+echo ""
+
+# Can setup other needed configuration such as environment variable
+# settings that may be needed by the computation.
+# For example, reduce tensorflow warning log level as these warning
+# are not really useful.
+export TF_CPP_MIN_LOG_LEVEL=3
+
+# Record start time to display elapsed time of computation
 echo "Start time: `date`"
 start=$(date +%s)
-conda activate keras-tf-gpu
-echo "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
-./gputest.py
+
+# Run the actual computation by invoking python script or executable
+python gputest.py
+
+# Record end time to display elapsed time of computation
 echo "End time: `date`"
 end=$(date +%s)
 secs=($end-$start)
